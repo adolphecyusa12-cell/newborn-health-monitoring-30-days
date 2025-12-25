db176ece9b0200980682e5e02eb300b1d5ace21d
adolphecyusa12-cell <adolphecyusa12@gmail.com>
2025-12-24 20:04:38 -0500

    Add R data loading script

---
+++ analysis/02_data_loading.R
@@ -0,0 +1,9 @@
+##### Load Tables from MySQL #####
+
+babies <- dbReadTable(con, "babies")
+excretion <- dbReadTable(con, "excretion")
+illness <- dbReadTable(con, "illness")
+immunization <- dbReadTable(con, "immunization")
+size <- dbReadTable(con, "size")
+vitals <- dbReadTable(con, "vitals")
+immunization_type <- dbReadTable(con, "immunization_type")
